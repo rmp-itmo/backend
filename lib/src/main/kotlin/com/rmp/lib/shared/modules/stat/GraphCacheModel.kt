cfead658b3dbc8d53f956d28ae71db0ca2b62800
dudosyka <a.hatson22@gmail.com>
2025-04-01 19:27:18 +0300

    fix(stat): graph getters fix

--- lib/src/main/kotlin/com/rmp/lib/shared/modules/stat/GraphCacheModel.kt
+++ lib/src/main/kotlin/com/rmp/lib/shared/modules/stat/GraphCacheModel.kt
@@ -1,9 +1,11 @@
 package com.rmp.lib.shared.modules.stat
 
+import com.rmp.lib.shared.modules.user.UserModel
 import com.rmp.lib.utils.korm.IdTable
 
 object GraphCacheModel: IdTable("graph_cache") {
     val name = text("graph_name")
+    val userId = reference("user_id", UserModel)
     val top = int("top_bound")
     val bottom = int("bottom_bound")
     val results = text("results")
